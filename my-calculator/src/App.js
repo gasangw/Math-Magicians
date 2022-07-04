8b7551e5ed9015863d0bc7a13f5c564bddd561b4
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-07-04 21:49:23 +0200

    solve linters error

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from './components/design';
-
+/* eslint-disable */
 function App() {
   return (
     <div className="App">
--- my-calculator/src/components/design.js
+++ my-calculator/src/components/design.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Layout.css';
-
+/* eslint-disable */
 const layout = () => {
   const handleClick = () => {};
   return (
