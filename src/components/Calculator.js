5f3dd68b31cfc4304c03e22e67e4b5612b72addf
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-07-07 15:31:02 +0200

    implement the function component and use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,130 +1,122 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Layout.css"
 import calculate from '../logic/calculate';
-class Structure extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      total: 0,
-      next: '',
-      operation: '',
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
 
-  handleClick = (event) => {
+const Structure = () => {
+  const [state, setState] = useState(0);
+  
+  function handleClick(event) {
     const buttonName = event.target.value;
-    this.setState((prevState) => calculate(prevState, buttonName));
+    setState((prevState) => calculate(prevState, buttonName));
+    
   }
-
-  render(){
   return (
     <div className="frame">
       <div className="calculator">
         <br />
         <div> 
-        <button className="screen">{this.state.total}{this.state.operation}{this.state.next}</button>      
+        <button className="screen">{state.total}{state.operation}{state.next}</button>      
         </div>
         <div className="keys">
           <input
             type="button"
             value="AC"
             className="button-clear"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="+/-"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="%"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="&#247;"
             className="button-operator"
             id="red"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="7"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="8"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="9"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="x"
             className="button-operator"
             id="red"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="4"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="5"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="6"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="-"
             className="button-operator"
             id="red"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="1"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="2"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="3"
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="+"
             className="button-operator"
             id="red"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
 
           <input
@@ -132,26 +124,25 @@ class Structure extends React.Component {
             value="0"
             className="button-operator"
             id="blue"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="."
             className="button-operator"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
           <input
             type="button"
             value="="
             className="button-operator"
             id="red"
-            onClick={this.handleClick}
+            onClick={handleClick}
           />
         </div>
       </div>
     </div>
   );
-  }
-};
+  };
 
 export default Structure;
